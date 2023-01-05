1717d43ec2cd9f85f38c95f795889f2746687801
Chaklun <info@aux.dp.ua>
2023-01-05 18:28:50 +0200

    update js

--- Lessons/L7/HW_7/vol.js
+++ Lessons/L7/HW_7/vol.js
@@ -1,7 +1,6 @@
-let button = document.querySelector('.buttonVol');
-var x = document.querySelector('.height');
-var y = document.querySelector('.diameter');
-button.onclick = function (){
-    result =  x.value * y.value;
-   document.querySelector('span.volume').innerHTML = result;
+document.querySelector('.buttonVol').onclick = function () {
+    x = document.querySelector('.height');
+    y = document.querySelector('.diameter');
+    result = x.value * y.value;
+    document.querySelector('span.volume').innerHTML = result;
 }

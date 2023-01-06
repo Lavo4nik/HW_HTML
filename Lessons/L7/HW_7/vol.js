079574d984b2e89870fac07d834288abc64ca5a3
Chaklun <info@aux.dp.ua>
2023-01-06 20:05:37 +0200

    rebuild

--- Lessons/L7/HW_7/vol.js
+++ Lessons/L7/HW_7/vol.js
@@ -1,11 +1,10 @@
-
 document.querySelector('.buttonVol').onclick = function () {
     x = document.querySelector('.height');
     y = document.querySelector('.diameter');
     result = x.value * y.value;
     document.querySelector('span.volume').innerHTML = "Объем " + result;
     result = 0;
-    document.querySelector('.height').reset;
+    newFunction();
 }
 document.querySelector('.say').onclick = function () {
     iName = document.querySelector('.inputName');
@@ -13,16 +12,16 @@ document.querySelector('.say').onclick = function () {
 }
 document.querySelector('.buttonPer').onclick = function () {
     x = document.querySelector('.side');
-    //квадрат
+    //степень
     //result = Math.pow(x.value, 2);
     result = x.value * 4;
-    document.querySelector('span.perimetre').innerHTML ="Периметр " + result;
+    document.querySelector('span.perimetre').innerHTML = "Периметр " + result;
     result = 0;
 }
 document.querySelector('.buttonSpeed').onclick = function () {
     x = document.querySelector('.distance');
     y = document.querySelector('.time');
     result = x.value / y.value;
-    document.querySelector('span.needSpeed').innerHTML ="Необходимая скорость " + result + "км/ч";
+    document.querySelector('span.needSpeed').innerHTML = "Необходимая скорость " + result + "км/ч";
     result = 0;
 }

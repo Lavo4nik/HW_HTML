376954edfc3a4fa6e81e8782cd99f9a1ec18935e
Chaklun <info@aux.dp.ua>
2023-01-06 19:45:21 +0200

    Done_2

--- Lessons/L7/HW_7/vol.js
+++ Lessons/L7/HW_7/vol.js
@@ -1,6 +1,28 @@
+
 document.querySelector('.buttonVol').onclick = function () {
     x = document.querySelector('.height');
     y = document.querySelector('.diameter');
     result = x.value * y.value;
-    document.querySelector('span.volume').innerHTML = result;
+    document.querySelector('span.volume').innerHTML = "Объем " + result;
+    result = 0;
+    document.querySelector('.height').reset;
+}
+document.querySelector('.say').onclick = function () {
+    iName = document.querySelector('.inputName');
+    document.querySelector('.greeting').innerHTML = "Привіт, " + iName.value + "!";
+}
+document.querySelector('.buttonPer').onclick = function () {
+    x = document.querySelector('.side');
+    //квадрат
+    //result = Math.pow(x.value, 2);
+    result = x.value * 4;
+    document.querySelector('span.perimetre').innerHTML ="Периметр " + result;
+    result = 0;
+}
+document.querySelector('.buttonSpeed').onclick = function () {
+    x = document.querySelector('.distance');
+    y = document.querySelector('.time');
+    result = x.value / y.value;
+    document.querySelector('span.needSpeed').innerHTML ="Необходимая скорость " + result + "км/ч";
+    result = 0;
 }

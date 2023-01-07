38cdb084baa72dd3f35642d08a33f45839b5bd01
Chaklun <info@aux.dp.ua>
2023-01-07 20:15:19 +0200

    Done

---
+++ Lessons/L9/HW_9/lucky.js
@@ -0,0 +1,28 @@
+document.querySelector('.lucky').onclick = function () {
+    num = document.querySelector('.inputNum').value;
+    if (num.length == 6) {
+        compute();
+        if (fChar == lChar) {
+            document.querySelector('.result').innerHTML = 'Повезло';
+        } else {
+            document.querySelector('.result').innerHTML = 'Не повезло';
+        }
+    } else {
+        document.querySelector('.result').innerHTML = 'Вы ввели неверный номер!';
+    }
+}
+function compute() {
+    var fNum = num.substr(0, 3);
+    var lNum = num.substr(3, 3);
+    fChar = 0;
+    lChar = 0;
+    for (i = 0; i <= 5; i++) {
+        if (i < 3) {
+            fChar = fChar + fNum % 10;
+            fNum = parseInt(fNum / 10);
+        } else {
+            lChar = lChar + lNum % 10;
+            lNum = parseInt(lNum / 10);
+        }
+    }
+}

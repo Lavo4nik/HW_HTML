29fc7de480f303a513610008a54678f709c5b9f3
Chaklun <info@aux.dp.ua>
2023-01-14 10:13:26 +0200

    stage

---
+++ Lessons/L11/HW_11_2/secret.js
@@ -0,0 +1,15 @@
+function qs(selector) {
+    return document.querySelector(selector);
+}
+qs('.result').onclick = function () {
+    let place = qs('.mesto').value;
+    /*     console.log(x); */
+    if (place > 54 || place <0) {
+        const number_coupe = place <= 36 ? Math.trunc((place - 1) / 4 + 1) : Math.trunc((54 - place) / 2 + 1);
+        const up_bottom = place % 2 == 0 ? 'Верхняя' : 'Нижняя';
+        qs('.result').innerText = 'Вы путушествуете на крыше';
+    }
+    else
+        qs('.result').innerText = `Ваше купе ${number_coupe} ${up_bottom} полка `;
+}
+

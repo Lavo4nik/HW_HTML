07959c422b828c48c4c4f0483aecda93de45ec47
Chaklun <info@aux.dp.ua>
2023-01-15 09:45:47 +0200

    11_All_Done_but_not_cleared

--- Lessons/L11/HW_11_1/vasya.js
+++ Lessons/L11/HW_11_1/vasya.js
@@ -6,17 +6,21 @@ qs('.result-line').onclick = function () {
     let y = qs('.ill').value;
     qs('.result-line').innerText = (`${(x + y) / 50 * 100} строк`);
 }
-/* !спросить  */
-qs('.result-line').onclick = function () {
-    x = qs('.2-lines').value;
-    y = qs('.2-salary').value;
-    qs('.2-result').innerText = (`${(x + y) / 50 * 100} строк`);
+/* Подигрываю разработчику, округляю в большую=) */
+qs('.result-2').onclick = function () {
+    let x = qs('.lines-2').value;
+    let y = qs('.salary-2').value;
+    let res = Math.round((y - (x / 100 * 50)) / 20);
+    if (res > 0) {
+        qs('.result-2').innerText = (`В запасе ${res}  опозданий`);
+    }
+    else
+        qs('.result-2').innerText = 'У Вас нет возможности опаздывать';
 }
 qs('.result-3').onclick = function () {
     let x = qs('.lines-3').value;
     let y = qs('.salary-3').value;
     res = (x / 100) * 50 - (y * 20);
-    console.log(res);
     if (res > 0) {
         qs('.result-3').innerText = (`Доход ${res}`);
     }

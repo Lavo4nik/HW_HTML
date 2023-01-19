a284e0bcfa7209dfb867e2e93c321eb36c204f63
Chaklun <info@aux.dp.ua>
2023-01-19 20:26:00 +0200

    Done_HW_15

--- Lessons/L15/HW_15/word.js
+++ Lessons/L15/HW_15/word.js
@@ -1,24 +1,65 @@
 function qs(selector) {
     return document.querySelector(selector);
 }
-
 qs('.solution-result').onclick = function () {
     let originalString = qs('.solution-input').value;
-    /* console.log(originalString); */
-    const wordsFromStrig = originalString.split(' ');
-    /* console.log(wordsFromStrig); */
-    /*  inputLength = wordsFromStrig.length; */
-    /* console.log(inputLength); */
+    originalString = originalString.trim();
+    let wordsFromStrig = originalString.split(' ');
     let result = wordsFromStrig[0].length;
-    console.log(result);
     for (let index = 0; index < wordsFromStrig.length; index++) {
-        console.log(index);
-        console.log(wordsFromStrig[index].length);
-        console.log(result);
-        if (result.length > wordsFromStrig[index].length) {
+        if (result > wordsFromStrig[index].length) {
             result = wordsFromStrig[index].length;
         }
-
-        
     }
+    qs('.solution-result').innerText = result;
+}
+
+qs('.dnk-solution-result').onclick = function () {
+    let originalString = qs('.dnk-solution-input').value;
+    originalString = originalString.toUpperCase();
+    let result = '';
+
+    /*! Сначала я делал так.... */
+    /*     for (let index = 0; index < originalString.length; index++) {
+    
+            let x = originalString.substring(index, index + 1);
+            console.log(x);
+            if (x == 'T') {
+                result += 'A';
+    
+            }
+            if (x == 'A') {
+                result += 'T';
+    
+            }
+            if (x == 'G') {
+                result += 'C';
+    
+            }
+            if (x == 'C') {
+                result += 'G';
+    
+            }
+    
+        } */
+        /* А потом я узнал об этом..... :((( */
+    for (const x of originalString) {
+
+        if (x == 'T') {
+            result += 'A';
+
+        }
+        if (x == 'A') {
+            result += 'T';
+
+        }
+        if (x == 'G') {
+            result += 'C';
+
+        }
+        if (x == 'C') {
+            result += 'G';
+        }
+    }
+    qs('.dnk-solution-result').innerText = result;
 }

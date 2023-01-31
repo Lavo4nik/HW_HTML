3e83f1dc477dda2d0c617697f8f2521ba9210a3a
YaEnotina <info@aux.dp.ua>
2023-01-31 20:07:48 +0200

    add HW 17

---
+++ Lessons/L17/HW_17_1/tik-tak-toe.js
@@ -0,0 +1,7 @@
+const stol = [[9, 7, 2], [0, 1, 2], [2, 1, 0]];
+console.log(stol);
+for (let index = 0; index < stol.length; index++) {
+    console.log(index);
+           /*  let summ += stol[0][index]; */
+            /* console.log(summ); */
+}

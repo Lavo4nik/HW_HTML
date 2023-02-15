7a984f43f8b45793cb22a9a2feeb4b33ed3d4314
YaEnotina <info@aux.dp.ua>
2023-02-15 20:31:22 +0200

    17_add

--- Lessons/L17/HW_17_1/tik-tak-toe.js
+++ Lessons/L17/HW_17_1/tik-tak-toe.js
@@ -1,7 +1,34 @@
-const stol = [[9, 7, 2], [0, 1, 2], [2, 1, 0]];
-console.log(stol);
+const stol = [[9, 1, 2],
+                [0, 1, 2],
+                [2, 2, 2]];
+/* console.log(stol); */
+
 for (let index = 0; index < stol.length; index++) {
-    console.log(index);
-           /*  let summ += stol[0][index]; */
-            /* console.log(summ); */
+    let summ = '';
+    for (let i = 0; i < stol[index].length; i++) {
+        summ += stol[index][i];
+        if (summ == '111') {
+            console.log('tra');
+        }
+        if (summ == '222') {
+            console.log('tra1');
+        }
+
+    }
+
+}
+
+for (let index = 0; index < stol[index].length; index++) {
+    console.log('Хоп', stol[index].length);
+    let summ ='';
+    for (let i = 0; i < stol.length; i++) {
+        summ += stol[i][index];
+        if (summ == '111') {
+            console.log('tra2');
+        }
+        if (summ == '222') {
+            console.log('tra3');
+        }
+    }
+
 }

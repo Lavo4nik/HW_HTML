023bc3d578f47fa3f131f0c5e6e184f81561f45d
YaEnotina <info@aux.dp.ua>
2023-02-17 20:49:37 +0200

    upd

--- Lessons/L17/HW_17_1/tik-tak-toe.js
+++ Lessons/L17/HW_17_1/tik-tak-toe.js
@@ -1,34 +1,25 @@
-const stol = [[9, 1, 2],
-                [0, 1, 2],
-                [2, 2, 2]];
-/* console.log(stol); */
-
+const stol = [[1, 1, 0], [0, 1, 2], [2, 0, 2]];
 for (let index = 0; index < stol.length; index++) {
     let summ = '';
     for (let i = 0; i < stol[index].length; i++) {
         summ += stol[index][i];
-        if (summ == '111') {
-            console.log('tra');
-        }
-        if (summ == '222') {
-            console.log('tra1');
-        }
-
+        score(summ);
     }
-
 }
-
 for (let index = 0; index < stol[index].length; index++) {
-    console.log('Хоп', stol[index].length);
-    let summ ='';
+    let summ = '';
     for (let i = 0; i < stol.length; i++) {
         summ += stol[i][index];
-        if (summ == '111') {
-            console.log('tra2');
-        }
-        if (summ == '222') {
-            console.log('tra3');
-        }
+        score(summ);
+    }
+}
+function score(summ) {
+    if (summ == '111') {
+        console.log('Winner X');
+        return;
+    }
+    if (summ == '222') {
+        console.log('Winner O');
     }
 
 }
